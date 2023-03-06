9159ce7842d270fa668c65d960f632b0dcf24aeb
王格格 <2461621477@qq.com>
2023-03-06 10:40:01 +0800

    eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly'
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json']
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
